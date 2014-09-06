09a24c216c365d2fa03d8bf236bb5487063aab88
Marco Lavagnino <marco.lavagnino@gmail.com>
2014-09-06 17:13:49 -0300

    organization model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Organization(models.Model):
+    name = models.CharField(max_length=100)
+    city_id = models.IntegerField()
+    address = models.CharField(max_length = 300)
+    ttl = models.DateTimeField()
+
