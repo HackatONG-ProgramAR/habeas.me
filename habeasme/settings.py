96af826c3d2afa93e1a88957873d233b8129c18c
MissFilly <lady.filly@gmail.com>
2014-09-06 14:40:38 -0300

    basic home page

--- habeasme/settings.py
+++ habeasme/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'habeasme.app',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,4 +81,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- habeasme/urls.py
+++ habeasme/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'habeasme.views.home', name='home'),
+    url(r'^$', 'habeasme.app.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
